f04e244f401815eda8a2a09f16fb679fc51885c8
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2024-04-29 00:36:31 +0530

    first commit

---
+++ BakeHouse/src/environment/config.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDiPmdhJ8HNcdSg8aO01UbBe6m6NC6rEto',
+    authDomain: 'icecream-b527d.firebaseapp.com',
+    projectId: 'icecream-b527d',
+    storageBucket: 'icecream-b527d.appspot.com',
+    messagingSenderId: '409246698538',
+    appId: '1:409246698538:web:73acfd2b5bd9412bc70753',
+  },
+};
